b3bf39602580f6d49c16763fe87211c7b5822d1a
Ricky Roberts <Richard.robertsf@gmail.com>
2024-10-29 02:11:37 -0400

    new chat design

--- cbt_chatbot/front-end/src/components/chatbot/Chatbot.js
+++ cbt_chatbot/front-end/src/components/chatbot/Chatbot.js
@@ -1,10 +1,12 @@
-// src/Chatbot.js
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
+import "./Chatbot.css"
 import axios from "axios";
+
 const Chatbot = () => {
   const [userInput, setUserInput] = useState("");
   const [conversation, setConversation] = useState([]);
-  const sendMessage = async () => {
+  const messagesEndRef = useRef(null);
+  const sendMessage = async () => {  
     if (userInput.trim()) {
       const newConversation = [
         ...conversation,
@@ -19,30 +21,45 @@ const Chatbot = () => {
         );
         setConversation([
           ...newConversation,
-          { text: response.data.message, sender: "bot" },
+          { text: response.data.message, sender: "AI" },
         ]);
       } catch (error) {
         console.error("Error sending message:", error);
       }
     }
   };
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+        messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+  useEffect(() => {
+    scrollToBottom();
+  }, [conversation]);
   return (
-    <div>
+    <div class="chat-window">
       <h1>Chat with our AI Bot</h1>
-      <div>
+      <div class="chat-messages">
+        <span class="start-message"> This is the beginning of your CBT chat session </span>
         {conversation.map((message, index) => (
-          <p key={index} className={message.sender}>
-            {message.text}
-          </p>
+          <div key={index} className={`${message.sender}-message message`}>
+            <p key={index} className={message.sender}>
+              {message.text}
+            </p>
+          </div>
         ))}
+        <span ref={messagesEndRef} />
       </div>
-      <input
-        type="text"
-        value={userInput}
-        onChange={(e) => setUserInput(e.target.value)}
-        onKeyPress={(e) => e.key === "Enter" && sendMessage()}
-      />
-      <button onClick={sendMessage}>Send</button>
+      <div class="input-container">
+        <input
+          type="text"
+          placeholder="Type your message here..."
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
     </div>
   );
 };
