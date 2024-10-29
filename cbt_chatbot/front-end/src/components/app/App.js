7639e697034b83d63045ab5db13e5856cf23a71e
Ricky Roberts <Richard.robertsf@gmail.com>
2024-10-29 02:32:58 -0400

    simple multi-page navigation

--- cbt_chatbot/front-end/src/components/app/App.js
+++ cbt_chatbot/front-end/src/components/app/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import "./App.css"
 import Chatbot from '../chatbot/Chatbot';
+import Home from '../home/Home'
 
 function App() {
   return (
-    <div className="App">
-      <Chatbot />
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/chatbot">Chatbot</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+      </Routes>
+    </Router>
   );
 }
 
