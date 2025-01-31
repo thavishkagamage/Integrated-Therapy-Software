dc57b972c2d8ff2c79c47e4cd9aebccc092ada0c
rickslick3 <richard.robertsf@gmail.com>
2025-01-31 12:45:11 -0500

    mock setup of a second tool

--- cbt_chatbot/back-end/backend_function_calls/tool_functions.py
+++ cbt_chatbot/back-end/backend_function_calls/tool_functions.py
@@ -1,2 +1,7 @@
+# Functions that correspond to the tools in tools.py
+
 def self_harm_help():
     return "you said something that may indicate self harm or harm to others. Please call 911 or go to your nearest emergency room."
+
+def start_cbt():
+    return "You said something that indicates you want to begin a CBT session"
