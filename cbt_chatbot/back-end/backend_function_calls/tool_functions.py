fb5c18d8cf120fc7bc0a77f8ddc143d1bd9abee9
rickslick3 <richard.robertsf@gmail.com>
2025-01-31 16:45:47 -0500

    removed response example in favor of debug line

--- cbt_chatbot/back-end/backend_function_calls/tool_functions.py
+++ cbt_chatbot/back-end/backend_function_calls/tool_functions.py
@@ -1,52 +1,5 @@
 import json
 
-# example response for detect_self_harm
-"""
-ChatCompletion(
-    id='chatcmpl-AvrKziGzfdAT6PxP39CnP4nKPvcaD', 
-    choices=[Choice(
-        finish_reason='tool_calls', 
-        index=0, 
-        ogprobs=None, 
-        message=ChatCompletionMessage(
-            content=None, 
-            refusal=None, 
-            role='assistant', 
-            audio=None, 
-            function_call=None, 
-            tool_calls=[ChatCompletionMessageToolCall(
-                id='call_RqzLaz6b69YwG7dkJOCRXFym', 
-                function=Function(
-                    arguments='{"user_response":"I want to die"}', 
-                    name='detect_self_harm'
-                ), 
-                type='function'
-            )]
-        )
-    )], 
-    created=1738353089, 
-    model='gpt-4o-2024-08-06', 
-    object='chat.completion', 
-    service_tier='default', 
-    system_fingerprint='fp_50cad350e4', 
-    usage=CompletionUsage(
-        completion_tokens=21, 
-        prompt_tokens=350, 
-        total_tokens=371, 
-        completion_tokens_details=CompletionTokensDetails(
-            accepted_prediction_tokens=0, 
-            audio_tokens=0, 
-            reasoning_tokens=0, 
-            rejected_prediction_tokens=0
-        ), 
-        prompt_tokens_details=PromptTokensDetails(
-            audio_tokens=0, 
-            cached_tokens=0
-        )
-    )
-)
-"""
-
 def detect_self_harm(**kwargs):
     user_response = kwargs.get('user_response', 'No user response provided')
     return f"You said '{user_response}', which indicates you may harm yourself or harm others. Please call 911 or go to your nearest emergency room."
@@ -66,6 +19,7 @@ def handle_response(message):
         # Extract function name and arguments from the message in the API response
         function_name = message.tool_calls[0].function.name
         function_args = json.loads(message.tool_calls[0].function.arguments)
+        print('\n' + str(function_name) + ' : ' + str(function_args) + '\n')
 
         # Check if function exists in mapping
         if function_name in function_mapping:
