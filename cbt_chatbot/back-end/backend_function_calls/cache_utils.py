f007e4a4c58a8321b2ea770881af9a638571058a
rickslick3 <richard.robertsf@gmail.com>
2025-02-13 16:04:25 -0500

    next steps

--- cbt_chatbot/back-end/backend_function_calls/cache_utils.py
+++ cbt_chatbot/back-end/backend_function_calls/cache_utils.py
@@ -5,17 +5,16 @@ import json
 from django.conf import settings
 from django.core.cache import cache
 
+
 def get_cache_file(cache_key):
     """
     Attempts to retrieve a file from the cache. If file is not in the cache, cache the file.
     Args:
         cache_key (str): identifier of the file in the cache (SAME AS FILE NAME without extension)
+                        the cache key is how we get a specific file from our cached memory
     Returns:
         prompts (json): The content of the json file contianing the prompts
     """
-    
-    # the cache key is how we get a specific file from our cached memory
-    # we can set this dynamically using DB values and pass it in as an argument here to retrieve files
 
     # get file from cache
     prompts = cache.get(cache_key)
@@ -29,12 +28,12 @@ def get_cache_file(cache_key):
 
         # Construct the full file path to your JSON file
         # BASE_DIR = /Integrated-Therapy-Software/cbt_chatbot/back-end
-        file_path = os.path.join(settings.BASE_DIR, 'backend_function_calls', 'static', file_name)
+        file_path = os.path.join(settings.BASE_DIR, 'backend_function_calls', 'prompts', file_name)
         
         try:
             with open(file_path, 'r') as json_file:
                 prompts = json.load(json_file)
-                
+
             # Cache the file with a long timeout (or use None)
             cache.set(cache_key, prompts, timeout=60 * 60 * 24 * 365 * 10) # 10 years in seconds
 
@@ -46,3 +45,13 @@ def get_cache_file(cache_key):
             prompts = {}
 
     return prompts
+
+
+# TODO: cache all important session files
+def cache_all_session_files(session_num):
+    """
+    Caches all files that will be used in the current session/mode to improve performance
+    Args:
+        session_num (int): identifies the session to help identify files to cache
+    """
+    # loop through static directory and cache all files that correlate to the session_num
