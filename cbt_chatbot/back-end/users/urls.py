11e4c3bb1cfd874a647ff1385dda8d4552971bcd
Thavishka Gamage <69032275+thavishkagamage@users.noreply.github.com>
2025-03-13 10:52:55 -0400

    users

--- cbt_chatbot/back-end/users/urls.py
+++ cbt_chatbot/back-end/users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import RegisterView, UserIdView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from .views import RegisterView, UserIdView, MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('tokenrefresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', UserIdView.as_view(), name='user-id'),
 ]
