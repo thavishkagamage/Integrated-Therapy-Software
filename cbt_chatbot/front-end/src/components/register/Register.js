af076af015b60dc516b5f39ae78c7ec914f49704
Grant Guernsey <guernsgd@mail.uc.edu>
2025-04-07 14:51:49 -0400

    Fixed link import

--- cbt_chatbot/front-end/src/components/register/Register.js
+++ cbt_chatbot/front-end/src/components/register/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import '../login/Login.css';
 
